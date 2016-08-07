a0bdfa4b948406fc63ef164d0684459aab8e8dab
Joshua Clark <joshua@clark.io>
2016-08-07 20:22:25 +0200

    Initial Commit

---
+++ standard-algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  standard-algorithms
+//
+//  Created by Joshua Clark on 07/08/2016.
+//  Copyright © 2016 Joshua Clark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
